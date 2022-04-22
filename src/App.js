466f3a9cdb273d84f394a2c93118636a1a202933
Andy Gardner <gardnerandy@gmail.com>
2022-04-22 15:46:06 +0000

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+    // src/App.js
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    import React, {Component} from 'react';
+    import Activities from './components/activities';
 
-export default App;
+    class App extends Component {
+
+      state = {
+        activities: []
+      }
+
+      componentDidMount() {
+        fetch('http://www.andygoesrunning.com:8080/garmin-activities')
+        .then(res => res.json())
+        .then((data) => {
+          this.setState({ activities: data })
+        })
+        .catch(console.log)
+      }
+
+      render () {
+        return (
+          <Activities activities={this.state.activities}/>
+        )
+      }
+    }
+
+    export default App;
