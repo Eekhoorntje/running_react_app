22e96afef16d3a9c46ace671086139e96be91f3b
Andy Gardner <gardnerandy@gmail.com>
2022-04-29 16:56:07 +0000

    Fixing stuff

--- src/components/activities.js
+++ src/components/activities.js
@@ -6,7 +6,7 @@
       return (
         <div>
           <center><h1>Activity List</h1></center>
-          {Activities.map((activity) => (
+          {activities.map((activity) => (
             <div class="card">
               <div class="card-body">
                 <h5 class="card-title">{activity.Title}</h5>
